4da380d623b7e43407426d0d07f4d34f3790dd83
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-06-11 16:11:53 +0200

    feat(App): create HierarchicalMenu

--- App.js
+++ App.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {
+  InstantSearch,
+  connectHierarchicalMenu,
+} from 'react-instantsearch-native';
+
+const Menu = ({ items, refine }) => (
+  <View>
+    {items.map(item => (
+      <View key={item.label}>
+        <TouchableOpacity onPress={() => refine(item.value)}>
+          <Text style={{ fontWeight: item.isRefined ? 'bold' : 'normal' }}>
+            {item.label} ({item.count})
+          </Text>
+        </TouchableOpacity>
+
+        {item.items && (
+          <View style={{ marginLeft: 25 }}>
+            {item.items.map(nest => (
+              <View key={nest.label}>
+                <TouchableOpacity onPress={() => refine(nest.value)}>
+                  <Text
+                    style={{ fontWeight: nest.isRefined ? 'bold' : 'normal' }}
+                  >
+                    {nest.label} ({nest.count})
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
+        )}
+      </View>
+    ))}
+  </View>
+);
+
+const ConnectedHierarchicalMenu = connectHierarchicalMenu(Menu);
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <InstantSearch
+          appId="DGZW8B2ESG"
+          apiKey="c422522bd6b85de6527a1cdaa0ea1e57"
+          indexName="dev_locations"
+        >
+          <ConnectedHierarchicalMenu attributes={['area', 'district']} />
+        </InstantSearch>
       </View>
     );
   }
